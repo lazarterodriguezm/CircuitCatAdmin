ff13a7815fcdc4129fcaa74ba039e1220853beb0
Marc <lazarterodriguezm@gmail.com>
2018-10-19 12:36:42 +0200

    Implementing QR code scanner

--- src/pages/scan-qr/scan-qr.ts
+++ src/pages/scan-qr/scan-qr.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ScanQrPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ScanQrPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private barcodeScanner: BarcodeScanner) {
   }
+
+  options : BarcodeScannerOptions = {
+    preferFrontCamera : false,
+    showFlipCameraButton : true,
+    showTorchButton : true,
+    disableAnimations : false,
+    disableSuccessBeep : false,
+    prompt : "Prompt",
+    formats : "QR_CODE",
+    orientation : "none",
+    torchOn : false,
+    resultDisplayDuration : 1500
+  };
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ScanQrPage');
+
+    this.barcodeScanner.scan(this.options).then(barcodeData => {
+      console.log('Cancelled: ', barcodeData.cancelled);
+      console.log('Format: ', barcodeData.format);
+      console.log('Text', barcodeData.text);
+    }).catch(err => {
+      console.log('Error', err);
+    });
   }
 
 }
